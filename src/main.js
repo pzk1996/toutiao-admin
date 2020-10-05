36682a245e7a94265cc63fbe556a401d41adfeb5
彭正坤 <1605616182@qq.com>
2020-10-05 21:54:50 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+//  安装less依赖
+//  npm install less less-loader --save-dev 解决Error: Can't resolve 'less-loader'
+import './styles/index.less'  //全局样式
 
 Vue.config.productionTip = false;
 
